4b213c4d209b92232a0e470a8c858aef7f4c58d5
AliiiBenn <davidddpereiraaa6@gmail.com>
2026-03-27 15:12:04 +0100

    feat(core): re-export zod from deesse package
    Co-Authored-By: martyy-code <nesalia.inc@gmail.com>

--- packages/deesse/src/index.ts
+++ packages/deesse/src/index.ts
@@ -4,3 +4,6 @@ export { defineConfig } from "./config";
 export type { Config } from "./config";
 export { plugin } from "./config";
 export type { Plugin } from "./config";
+
+export { z } from "zod";
+export type { ZodSchema } from "zod";
